2b5cd7effbbc935d121b731201e199353d26821e
kirill12345678900 <kirill.steam123456@gmail.com>
2023-01-17 22:08:00 +0300

    2 commit

--- src/main/java/by/kirill/spring/HelloController.java
+++ src/main/java/by/kirill/spring/HelloController.java
@@ -4,6 +4,7 @@ package by.kirill.spring;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class HelloController {
     @GetMapping("/hello")
